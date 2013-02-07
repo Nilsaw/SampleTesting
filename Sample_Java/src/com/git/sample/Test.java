be64f376016b17ec3a4cb1ade8ebd0f07ccc36f1
nilsaw <nilsaw@ubuntu>
2013-02-07 07:08:59 -0600

    Committing data testing.

---
+++ Sample_Java/src/com/git/sample/Test.java
@@ -0,0 +1,13 @@
+package com.git.sample;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Setting up a sample project to configure Eclipse with GitHUB");
+	}
+
+}
